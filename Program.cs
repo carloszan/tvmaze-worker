8953c34e0292c8878bb191785f039cf7b77e7bf3
Carlos <carloshlzansavio@gmail.com>
2022-09-15 17:37:54 +0100

    add singleton running

--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@ using tvmaze_worker;
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-        services.AddHostedService<Worker>();
+      services.AddHostedService<Worker>();
     })
     .Build();
 
-await host.RunAsync();
+await host.StartAsync();
--- Worker.cs
+++ Worker.cs
@@ -1,21 +1,23 @@
 namespace tvmaze_worker
 {
-    public class Worker : BackgroundService
+  public class Worker : IHostedService
+  {
+    private readonly ILogger<Worker> _logger;
+
+    public Worker(ILogger<Worker> logger)
     {
-        private readonly ILogger<Worker> _logger;
-
-        public Worker(ILogger<Worker> logger)
-        {
-            _logger = logger;
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
-        }
+      _logger = logger;
     }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+      _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+      await Task.Delay(1000, cancellationToken);
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+      return Task.CompletedTask;
+    }
+  }
 }
