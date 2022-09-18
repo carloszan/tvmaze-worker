ddde37c1fca323fd517963d596d6d894a0e57d72
Carlos <carloshlzansavio@gmail.com>
2022-09-18 18:32:53 +0100

    add invalid page test

--- TvMaze.Tests/Services/TvMazeScraperService_Test.cs
+++ TvMaze.Tests/Services/TvMazeScraperService_Test.cs
@@ -5,7 +5,7 @@ using Newtonsoft.Json;
 using TvMazeWorker.Services.Dtos;
 using TvMazeWorker.Services;
 
-namespace TvMazeTests.TvMazeScraper
+namespace TvMazeTests.Services
 {
   public class TvMazeScraperServiceTest 
   {
@@ -15,7 +15,7 @@ namespace TvMazeTests.TvMazeScraper
     }
 
     [Test]
-    public async Task MazeScraper_WithValidPage_ReturnsShows()
+    public async Task TvMazeScraper_WithValidPage_ReturnsShows()
     {
       var mockData = new List<ShowDto>() { new ShowDto() { Id = 1, Name = "Game of Thrones" } };
 
@@ -51,6 +51,42 @@ namespace TvMazeTests.TvMazeScraper
       var shows = await service.GetShowsAsync(1);
       // Assert
       Assert.IsNotEmpty(shows);
+    }
+
+    [Test]
+    public async Task TvMazeScraper_WithInvalidPage_ReturnsNull()
+    {
+      HttpResponseMessage resultMock = new HttpResponseMessage
+      {
+        StatusCode = System.Net.HttpStatusCode.NotFound,
+      };
+
+      var loggerStub = new Mock<ILogger<Worker>>();
+      var httpClientFactoryStub = new Mock<IHttpClientFactory>();
+
+      var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+
+      mockHttpMessageHandler
+        .Protected()
+        .Setup<Task<HttpResponseMessage>>(
+              "SendAsync",
+              ItExpr.IsAny<HttpRequestMessage>(),
+              ItExpr.IsAny<CancellationToken>()
+          )
+          .ReturnsAsync(resultMock)
+          .Verifiable();
+
+      var client = new HttpClient(mockHttpMessageHandler.Object);
+      httpClientFactoryStub
+        .Setup(_ => _.CreateClient(It.IsAny<string>()))
+        .Returns(client);
+
+      var service = new TvMazeScraperService(loggerStub.Object, httpClientFactoryStub.Object);
+
+      // Act
+      var shows = await service.GetShowsAsync(1);
+      // Assert
+      Assert.IsNull(shows);
     }
   }
 }
