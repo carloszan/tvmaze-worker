11552634476eab64bce478494f9db3ad428853f2
Carlos <carloshlzansavio@gmail.com>
2022-09-20 18:42:01 +0100

    refactor some tests

--- TvMaze.Tests/Repositories/ShowRepository_Test.cs
+++ TvMaze.Tests/Repositories/ShowRepository_Test.cs
@@ -14,25 +14,7 @@ namespace TvMazeTests.Repositories
       var lastId = 1;
       var fakeShows = new List<ShowEntity>() { new ShowEntity { Id = lastId, Name = "Game of Thrones", Cast = null } };
 
-      var showCollection = new Mock<IMongoCollection<ShowEntity>>();
-      showCollection.Object.InsertMany(fakeShows);
-
-      var mockCursor = new Mock<IAsyncCursor<ShowEntity>>();
-      mockCursor.Setup(x => x.Current).Returns(fakeShows);
-      mockCursor.SetupSequence(_ => _.MoveNext(It.IsAny<CancellationToken>())).Returns(true);
-
-      showCollection.Setup(x => x.FindAsync(
-        It.IsAny<FilterDefinition<ShowEntity>>(),
-        It.IsAny<FindOptions<ShowEntity, ShowEntity>>(),
-        It.IsAny<CancellationToken>()
-        )
-      ).ReturnsAsync(mockCursor.Object);
-
-      var dbMock = new Mock<IMongoDatabase>();
-      dbMock.Setup(_ => _.GetCollection<ShowEntity>(It.IsAny<string>(), default)).Returns(showCollection.Object);
-
-      var mongoClientMock = new Mock<IMongoClient>();
-      mongoClientMock.Setup(_ => _.GetDatabase(It.IsAny<string>(), default)).Returns(dbMock.Object);
+      Mock<IMongoClient> mongoClientMock = MongoClientFactory(fakeShows);
 
       var showRepository = new ShowRepository(mongoClientMock.Object);
 
@@ -50,6 +32,19 @@ namespace TvMazeTests.Repositories
       var lastId = 1;
       var fakeShows = new List<ShowEntity>() { new ShowEntity { Id = lastId, Name = "Game of Thrones", Cast = null } };
 
+      Mock<IMongoClient> mongoClientMock = MongoClientFactory(fakeShows);
+
+      var showRepository = new ShowRepository(mongoClientMock.Object);
+
+      // Act
+      var shows = await showRepository.GetShowsWithoutCastAsync();
+
+      // Assert
+      Assert.That(shows.FirstOrDefault().Cast, Is.Null);
+    }
+
+    private static Mock<IMongoClient> MongoClientFactory(List<ShowEntity> fakeShows)
+    {
       var showCollection = new Mock<IMongoCollection<ShowEntity>>();
       showCollection.Object.InsertMany(fakeShows);
 
@@ -69,14 +64,7 @@ namespace TvMazeTests.Repositories
 
       var mongoClientMock = new Mock<IMongoClient>();
       mongoClientMock.Setup(_ => _.GetDatabase(It.IsAny<string>(), default)).Returns(dbMock.Object);
-
-      var showRepository = new ShowRepository(mongoClientMock.Object);
-
-      // Act
-      var shows = await showRepository.GetShowsWithoutCastAsync();
-
-      // Assert
-      Assert.That(shows.FirstOrDefault().Cast, Is.Null);
+      return mongoClientMock;
     }
   }
 }
